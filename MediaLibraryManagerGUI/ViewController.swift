079fc1fecd621a10ceb2992dc3a41441307e2efd
Sam Paterson <>
2018-09-20 18:07:27 +1200

    Started UI, Completed categorys, working on file table view.
    Done table view but the string isnt changing "talk to paul" need to
    figure out how to do the preview thing next.

--- MediaLibraryManagerGUI/ViewController.swift
+++ MediaLibraryManagerGUI/ViewController.swift
@@ -9,10 +9,14 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+    @IBOutlet weak var fileTable: NSTableView!
+    
+    var files  = ["image 1", "image 2", "image 3", "image 4"]
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        fileTable.dataSource = self
+        fileTable.delegate = self
         // Do any additional setup after loading the view.
     }
 
@@ -22,6 +26,36 @@ class ViewController: NSViewController {
         }
     }
 
+    @IBAction func changeCategoryAction(_ sender: NSButton) {
+        if(sender.tag == 0){
+            //image button
+            print("image button pressed")
+        }else if (sender.tag == 1){
+            //video button
+            print("video button pressed")
+        }else if (sender.tag == 2){
+            //music button
+            print("music button pressed")
+        }else{
+            //other button
+            print("other button pressed")
+        }
+    }
+    
+}
 
+extension ViewController: NSTableViewDataSource, NSTableViewDelegate{
+    //dataSource function
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return files.count
+    }
+    
+    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+        print("making cell \(row)")
+        let file = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as! NSTableCellView
+        file.textField!.stringValue = files[row]
+        print("file text \(file.textField!.stringValue)")
+        return file
+    }
 }
 
