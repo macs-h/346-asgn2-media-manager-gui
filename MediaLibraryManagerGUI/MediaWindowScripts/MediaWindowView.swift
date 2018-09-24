45b32c412d68c9d58dfa6c0409c69c29ec6b1343
Max Huang <git@maxhuang.nz>
2018-09-25 11:19:34 +1200

    Fixed compile sources

---
+++ MediaLibraryManagerGUI/MediaWindowScripts/MediaWindowView.swift
@@ -0,0 +1,19 @@
+//
+//  MediaWindowView.swift
+//  MediaLibraryManager
+//
+//  Created by Max Huang on 25/09/18.
+//  Copyright © 2018 Fire Breathing Rubber Duckies. All rights reserved.
+//
+
+import Cocoa
+
+class MediaWindowView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
---
+++ MediaLibraryManagerGUI/MediaWindowScripts/MediaWindowViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MediaWindowViewController.swift
+//  MediaLibraryManager
+//
+//  Created by Max Huang on 25/09/18.
+//  Copyright © 2018 Fire Breathing Rubber Duckies. All rights reserved.
+//
+
+import Cocoa
+
+class MediaWindowViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
