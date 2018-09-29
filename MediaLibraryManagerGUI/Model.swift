c351ff1a8d6a3f7e9c8031b69553715a0c63beca
Sam Paterson <sammo7531@gmail.com>
2018-09-29 16:55:49 +1200

    Delegates from Model done, Made model a singleton, added play skeleton functions

--- MediaLibraryManagerGUI/Model.swift
+++ MediaLibraryManagerGUI/Model.swift
@@ -8,13 +8,49 @@
 
 import Foundation
 
+
+protocol openFileModelDegate {
+    func updateOutets(currentFile: MMFile, notes: String, bookmarks: [float_t])
+    func openMedia(file: MMFile)
+}
+protocol mainViewModelDegate {
+    func updateOutets(CurrentFile: MMFile, files: [MMFile], fileType: String)
+}
+
+
 class Model{
+    static var instance = Model()
     var libary = MM_Collection()//holds all the files
-    var collection = MM_Collection() //holds the files that are show on screen (using categories)
-    
+    var subLibary = MM_Collection() //holds the files that are show on screen (using categories)
+    var currentFile = MM_File()
+    var openFileDelegate: openFileModelDegate?{
+        didSet{
+            updateOpenFileVC()
+        }
+    }
+    var mainViewDegate: mainViewModelDegate?{
+        didSet{
+            updateMainVC()
+        }
+    }
     
     func openFile(file: MMFile){
         //check the type of file and open it accordingly
-        
+        openFileDelegate?.openMedia(file: currentFile)
+    }
+    
+    func addBookmark(){
+        //get current time from the player
+        var time = 1.1 //temp
+        //add the metadata to the file
+        updateOpenFileVC()
+    }
+    
+    private func updateOpenFileVC(){
+        openFileDelegate?.updateOutets(currentFile: currentFile, notes: "this is the notes", bookmarks: [0.0, 0.1, 0.2])
+    }
+    
+    private func updateMainVC(){
+        mainViewDegate?.updateOutets(CurrentFile: currentFile , files: subLibary.collection, fileType: currentFile.fileType)
     }
 }
